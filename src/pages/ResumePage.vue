0719cfd3669dc092c1d32a0bb804f60ec8dbba42
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-06-28 14:37:01 -0600

    init commit

---
+++ src/pages/ResumePage.vue
@@ -0,0 +1,372 @@
+<template>
+  <section class="container-fluid elevation-5">
+    <section class="resume card shadow rounded-1 w-100" style="">
+      <div class="row d-flex">
+
+        <div class="col-4 bg-left text-center" style="background-color: rgba(49, 95, 243, 0.949);">
+
+          <div class="row">
+            <div class="col-12">
+              <img src="https://media.licdn.com/dms/image/D5603AQE1oMhSUgp7eQ/profile-displayphoto-shrink_800_800/0/1686836283314?e=1693440000&v=beta&t=sCT3w6gETah9CvOsjMMcaPnhffQ2gCvVUZsQfK96G_U" class="img-fluid myCoverImg elevation-5" style="" alt="logo" />
+            </div>
+          </div>
+
+          <div class="row mt-5 justify-content-center align-items-center">
+            <div class="col-10 card elevation-5 m-2" style="background-color: #39456b98;">
+              <h5 class="card-header header-left text-light"><u>About Me</u></h5>
+              <p class="small card-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae dolores exercitationem temporibus voluptatum modi amet sunt, facilis optio accusantium, a ratione deleniti eligendi. Consequatur numquam possimus omnis in commodi sunt.</p>
+            </div>
+          </div>
+
+          <div class="row justify-content-center align-items-center">
+            <div class="col-10 card elevation-5 m-2" style="background-color: #d5dae898;">
+              <h5 class="card-header header-left"><u>Hobbies</u></h5>
+              <div class="row card-body card-text card-left justify-content-around">
+                <div class="col-4 fs-3 m-1">
+                  <i class="mdi mdi-laptop" style="color: #158cfc; text-shadow: 1px 1px 3px black;"></i>
+                </div>
+                <div class="col-4 fs-3 m-1">
+                  <i class="mdi mdi-human-male-female-child shadow-lg elevation-5" style="color: skyblue; text-shadow: 1px 1px 3px black;"></i>
+                </div>
+                <div class="col-4 fs-3 m-1">
+                  <i class="mdi mdi-forest" style="color: #0e6c0e; text-shadow: 1px 1px 3px black;"></i>
+                </div>
+                <div class="col-4 fs-3 m-1">
+                  <i class="mdi mdi-handshake" style="color: #af9f77; text-shadow: 1px 1px 3px black;"></i>
+                </div>
+                <div class="col-4 fs-3 m-1">
+                  <i class="mdi mdi-atom" style="color: #00ffaa; text-shadow: 1px 1px 3px black;"></i>
+                </div>
+                <div class="col-4 fs-3 m-1">
+                  <i class="mdi mdi-account-multiple" style="color: #7109a5; text-shadow: 1px 1px 3px black;"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row justify-content-center align-items-center">
+            <div class="col-10 card mx-2" style="background-color: #39456b98;">
+              <h5 class="card-header header-left mb-0"><u>Contact</u></h5>
+              <div class="card-body card-text card-left">
+                <div class="d-flex justify-content-center">
+                  <i class="mdi mdi-card-account-mail text-light text-center fs-2 pb-2"></i>
+                  <p class="fw-bold ms-3 mt-2"> : wanderwithme1988@gmail.com</p>
+                </div>
+                <article class="fs-1 d-flex justify-content-between align-items-center">
+                  <p><a href="https://www.linkedin.com/in/anthonyvancattenburch/" role="button" class="card-text card-left"><i class="mdi mdi-linkedin" style="color: #1e90ff;"></i></a></p>
+                  <p><a href="https://github.com/AJVancattenburch" role="button" class="card-text card-left"><i class="mdi mdi-github" style="color: #1e1d2c;"></i></a></p>
+                  <p><a href="www.instagram.com/" role="button" class="card-text card-left"><i class="mdi mdi-instagram" style="color: #ff1e87;"></i></a></p>
+                </article>
+                <div class="d-flex justify-content-center">
+                  <i class="mdi mdi-cellphone text-dark fs-2"></i>
+                  <p class="fw-bold mt-2">: (208) 637-5436</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row justify-content-center align-items-center">
+            <div class="col-10 card m-2" style="background-color: #39456b98;">
+              <h5 class="card-header header-left text-light"><u>Interests</u></h5>
+              <div class="row card-body">
+                <div class="col-6 card-text card-left">
+                  <p>Lorem Ipsum</p>
+                  <p>Lorem Ipsum</p>
+                  <p>Lorem Ipsum</p>
+                </div>
+                <div class="col-6 card-text card-left">
+                  <p>Lorem Ipsum</p>
+                  <p>Lorem Ipsum</p>
+                  <p>Lorem Ipsum</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row justify-content-center align-items-center">
+            <div class="card github-card">
+              <div class="col-12 my-3">
+                <h4 class="card-header header-left bg-accent-secondary">Current Github Activity {April - July}</h4>
+                <div class="row ps-3">
+                  <div class="col-6">
+                    <img src="src/assets/img/githubCommitStatus.jpeg" class="img-fluid rounded-2" style="height: 200px; aspect-ratio: 1/1;" alt="githubStatusImg">
+                  </div>
+                  <div class="col-6">
+                    <img src="src/assets/img/githubCommitsGraph.jpeg" class="img-fluid rounded-2" style="height: 200px; aspect-ratio: 1/1;" alt="githubGraphImg">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+
+        <section class="col-8 bg-right">
+
+          <h1 class="my-name">Anthony Vancattenburch</h1>
+          <div class="bg-profession-tag py-2">
+            <h3><em>Full Stack Developer</em></h3>
+          </div>
+
+          <div class="row ps-4 mt-2">
+            <div class="col-12">
+              <h2 class="mt-3 text-uppercase card-header header-right"><u>career objective</u></h2>
+              <p class="text-justify card-text card-right">Lorem ipsum</p>
+            </div>
+          </div>
+
+          <div class="row px-1 mt-5">
+
+            <div class="col-6 card experience-card shadow mx-2 ms-5" style="width: 45%;">
+              <h2 class="card-header header-right mt-2">Experience</h2>
+              <div class="col-12">
+                <p class="text-uppercase card-text card-right bg-accent-small p-1 px-3 my-2 rounded-2">Company Name</p>
+                <p class="text-uppercase card-text card-right bg-accent-small p-1 px-3 my-2 rounded-2">City, State</p>
+                <p class="text-uppercase card-text card-right bg-accent-small p-1 px-3 my-2 rounded-2 mb-3">Employed from {date} to {date}</p>
+                  <ul class="card-right ps-5">
+                    <li class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</li>
+                    <li class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</li>
+                    <li class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</li>
+                  </ul>
+              </div>
+            </div>
+            
+            <div class="col-6 card experience-card shadow pb-3 ms-2" style="width: 45%;">
+              <h2 class="card-header header-right mb-0 mt-2">Projects</h2>
+
+              <div class="row">
+                <div class="col-12">
+
+                  <article class="">
+                      <h5 class="card-header header-right bg-accent">Game Pursuit { Team Capstone }</h5>
+      
+                        <p class="card-text card-right">
+                          Esports Platform. Gamers can sign up, be rewarded for their skills, and spend tokens to initiate tournaments and play for prizes, sponsor players for prizes, and grow their notability as a player.
+                          <br>
+                          <a href="https://github.com/ChanthaKammer/capstone" target="_blank" class="ps-3" rel="noopener noreferrer">Github Link</a>
+                        </p>
+                      
+  
+                      <h5 class="card-header header-right bg-accent">Cohort Final</h5>
+                      
+                        <p class="card-text card-right">
+                          Cohort Final Description
+                          <br>
+                          <a href="repo-link" target="_blank" class="ps-3" rel="noopener noreferrer">Github Link</a>
+                        </p>
+                      
+  
+                      <h5 class="card-header header-right bg-accent">Tower</h5>
+                      
+                        <p class="card-text card-right">
+                          Event Ticket Selling Platform for buying any kind of event ticket. Account holders can also post events, and interact with others that are attending events they want to attend.
+                          <br>
+                          <a href="https://github.com/AJVancattenburch/tower" target="_blank" class="ps-3" rel="noopener noreferrer">Github Link</a>
+                        </p>
+                      
+                  </article>
+
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row ps-4 mt-5">
+            <div class="col-12">
+              <h2 class="mt-3 text-uppercase card-header"><u>Education</u></h2>
+              <h3 class="card-text card-right">Boise CodeWorks - Full Stack Certification</h3>
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+              <p class=""></p>
+            </div>
+          </div>
+
+          <div class="row ps-4 mt-5">
+
+            <div class="col-6">
+              <h2 class="card-header header-right mt-3 text-uppercase mb-4"><u>Certifications</u></h2>
+              <p class="text-uppercase card-text card-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+              <p class="text-uppercase card-text card-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+              <p class="text-uppercase card-text card-right mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+            </div>
+
+            <div class="col-6">
+              <h2 class="card-header header-right mt-3 text-uppercase mb-4"><u>Technical Proficiencies</u></h2>
+
+              <article>
+                <div class="col-12 fw-bolder">HTML5</div>
+                <div class="progress my-3" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar bg-danger" style="width: 100%"></div>
+                </div>
+  
+                <div class="progress my-3" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="">
+                  <div class="progress-bar bg-success" style="width: 25%">SQL</div>
+                </div>
+  
+                <div class="progress my-3" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar bg-info text-dark" style="width: 50%"></div>
+                </div>
+  
+                <div class="progress my-3" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar bg-warning text-dark" style="width: 75%"></div>
+                </div>
+  
+                <div class="progress my-3" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="">
+                  <div class="progress-bar bg-success" style="width: 25%"></div>
+                </div>
+  
+                <div class="progress my-3" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar bg-info text-dark" style="width: 50%"></div>
+                </div>
+  
+                <div class="progress my-3" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar bg-warning text-dark" style="width: 75%"></div>
+                </div>
+  
+                <div class="progress my-3" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar bg-danger" style="width: 100%"></div>
+                </div>
+              </article>
+
+            </div>
+
+          </div>
+
+        </section>
+
+      </div>
+    </section>
+  </section>
+
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+.resume {
+  background-color: aliceblue;
+  border-radius: 10px;
+}
+
+.my-name {
+  color: rgb(0, 102, 255);
+  text-shadow: 1px 1px 2px black;
+}
+
+.bg-left, .header-left {
+  color: aliceblue;
+  text-shadow: 1px 1px 3px black;
+}
+
+.card {
+  box-shadow: 5px 5px 5px #202325;
+}
+
+.card-header {
+  margin-bottom: 1rem;
+}
+
+.card-left {
+  color: aliceblue;
+  text-shadow: 1px 1px 2px black;
+}
+
+.card-right, p.card-right {
+  color: black;
+  font-weight: 500;
+  text-shadow: .5px .5px 1px dodgerblue;
+  padding-left: 2rem;
+}
+
+.github-card {
+  background-color: #39456b98;
+  box-shadow: 5px 5px 5px #202325;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 90%;
+  height: 90%;
+}
+
+.experience-card {
+  background-color: #beddf86b;
+  box-shadow: 5px 5px 5px #202325;
+  display: flex;
+}
+
+.bg-right {
+  background-color: #dcecfa;
+}
+
+.header-right {
+  color: black;
+  text-shadow: 1px 1px 3px white;
+  background-color: #0a43fe2e;
+  border-radius: .5rem;
+  padding-left: 2rem;
+}
+
+.bg-accent {
+  background-color: #0a43fe15;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  margin-left: 1rem;
+  padding-right: 2rem;
+}
+
+.bg-accent-small {
+  background-color: #0a43fe15;
+  margin-left: 1rem;
+}
+
+.bg-accent-secondary {
+  background-color: #570afe93;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  margin-left: 1rem;
+  padding-right: 2rem;
+  padding-left: 2rem;
+}
+
+.myCoverImg {
+  width: 250px;
+  height: 250px;
+  background-position-x: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin-top: 5vh;
+  margin-left: 1rem;
+  clip-path: polygon(0% 0%, 60% 0%, 60% 0%, 100% 50%, 60% 100%, 60% 100%, 0% 100%);
+}
+
+.bg-profession-tag {
+  color: black;
+  text-shadow: 1px 1px 3px white;
+  background-color: #0a43fefb;
+  border-radius: .5rem;
+  padding-left: 2rem;
+  margin-top: 1rem;
+  width: 75%;
+  clip-path: polygon(3% 0%, 60% 0%, 60% 0%, 100% 0%, 97% 100%, 60% 100%, 0% 100%);
+  box-shadow: 10px 0px 10px 10px inset #dcecfa;
+}
+
+.email {
+  font-size: .87rem;
+}
+
+</style>
